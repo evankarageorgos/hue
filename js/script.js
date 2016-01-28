16956a291e0749e1698694216be911cee16ed033
evan <vangelis.karageorgos@gmail.com>
2016-01-28 13:50:41 +0200

    Demo page

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// https://jsfiddle.net/xcEa9/74/
+// $(document).ready(function($){
+//
+//     $("backgroundselect").change(function(){
+//        var s = $(this);
+//        var name = s.attr('name');
+//         $('.'+name).removeClass().addClass(name+' '+s.find(':selected').text().toLowerCase());
+//         console.log(s.find(':selected').text());
+//     });
+// });
+
+//  http://jsfiddle.net/9PeAc/1/
+
+$(document).ready(function() {
+
+var selectedScheme = 'theBridge';
+
+$('#color_scheme').change(function(){
+    $('body').removeClass(selectedScheme).addClass($(this).val());
+    selectedScheme = $(this).val();
+});
+});
+
+
+
+// Testing
+
+// $(document).ready(function() {
+//     $("#co").click(function() {
+//         $("body").css("background-color","blue"); //edit, body must be in quotes!
+//     });
+// });
